e674899f30ebf279eff45e7c5006ab656efc4a06
Abhaay93 <abhayhipparkar97@gmail.com>
2025-07-31 04:07:29 +0530

    Commit message

--- data/projects.ts
+++ data/projects.ts
@@ -9,32 +9,31 @@ export type Project = {
 }
 
 export const projects: Project[] = [
-    {
-        title: "Pokedex",
-        description: "Pokedex.",
-        imgPath: "/project-images/pokedex.png",
-        techs: ["ReactJS", "NextJS","ExpressJS" , "Shadcn UI", "TailwindCSS", "MongoDB", ],
-        githubLink: "",
-        liveLink: "",
-        status: 'online'
-    },
-    {
-        title: "Martian Snaps",
-        description: "Mars rover images.",
-        imgPath: "/project-images/MartianSnaps.png",
-        techs: ["ReactJS", ,"Shadcn UI",   "TailwindCSS", "Nasa API"],
-        githubLink: "",
-        liveLink: "",
-        status: 'online'
-    },
-    {
-        title: "Drowa",
-        description: "excalidraw Clone.",
-        imgPath: "/project-images/drowa.png",
-        techs: ["Websocket", "NextJS","TypeScript" ,  "Supabase", ],
-        githubLink: "",
-        liveLink: "",
-        status: 'building'
-    }
-   
-]
+  {
+    title: "Pokedex",
+    description: "Pokedex.",
+    imgPath: "/project-images/pokedex.png",
+    techs: ["ReactJS", "NextJS", "ExpressJS", "Shadcn UI", "TailwindCSS", "MongoDB"],
+    githubLink: "",
+    liveLink: "",
+    status: "online",
+  },
+  {
+    title: "Martian Snaps",
+    description: "Mars rover images.",
+    imgPath: "/project-images/MartianSnaps.png",
+    techs: ["ReactJS", "Shadcn UI", "TailwindCSS", "Nasa API"], // <- fixed
+    githubLink: "",
+    liveLink: "",
+    status: "online",
+  },
+  {
+    title: "Drowa",
+    description: "excalidraw Clone.",
+    imgPath: "/project-images/drowa.png",
+    techs: ["Websocket", "NextJS", "TypeScript", "Supabase"],
+    githubLink: "",
+    liveLink: "",
+    status: "building",
+  },
+];
